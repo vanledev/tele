b5e62651fe4869058c46d59bd52a9934aa30f52d
vanledev <36130732+vanledev@users.noreply.github.com>
2024-01-13 17:26:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ server.use(express.json());
 server.get("/", (req, res) => {
   res.send("hi");
 });
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 server.post("/", async (req, res) => {
   console.log("Received POST request with data:", req.body);
   const group_ids = req.body.group_ids;
